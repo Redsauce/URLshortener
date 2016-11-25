8869ad1fa771ff68d8c255a4da53ac986a44d6dd
Kenan Rhoton <krhoton@redsauce.net>
2016-11-25 11:59:34 +0100

    Disabled Debug (doh!) and added short.redsauce.net as an allowed host (double doh!)

--- django/src/src/settings.py
+++ django/src/src/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+1e2ipa7xa%=9%!rj(ot9vof!=&#%!$tn7&_fdh_!llrk!#$rm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['short.redsauce.net']
 
 
 # Application definition
