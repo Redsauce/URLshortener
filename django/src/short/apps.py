ef4f8be9b018b7019f4ff0e33b62edb5393a05ac
Kenan Rhoton <krhoton@redsauce.net>
2016-12-19 14:05:14 +0100

    Removed OLD Django files

--- django/src/short/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShortConfig(AppConfig):
-    name = 'short'
--- django/src/startup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-python manage.py collectstatic --noinput
-gunicorn src.wsgi -w 2 -b 0.0.0.0:8000
